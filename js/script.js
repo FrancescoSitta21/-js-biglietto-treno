f82d35e6ecd7d20654ba93acc8d285d2e28d3a4e
Francesco <sittafrancesco2020@gmail.com>
2020-05-20 15:05:14 +0200

    Inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var etaPasseggero = prompt('Quanti anni hai?');
+console.log(etaPasseggero);
+
+var distanza = prompt('Quanti chilometri devi percorrere?');
+console.log(distanza);
+
+var costo = 0.21
+
+var costocomplessivo = (costo * distanza);
+console.log(costocomplessivo);
